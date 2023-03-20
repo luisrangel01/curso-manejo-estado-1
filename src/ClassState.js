4a58369942ad2e92809af72dd2413c42e5cb2533
Luis Rangel <luis.rangel@telus.com>
2023-03-20 16:14:41 -0600

    feat: simple state

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+const SECURITY_CODE = "paradigma";
+
 class ClassState extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      value: "",
       error: false,
       loading: false,
     };
@@ -28,7 +31,11 @@ class ClassState extends React.Component {
     if (this.state.loading) {
       setTimeout(() => {
         console.log(`haciendo la validacion`);
-        this.setState({ loading: false });
+        if (this.state.value === SECURITY_CODE) {
+          this.setState({ loading: false });
+        } else {
+          this.setState({ loading: false, error: true });
+        }
         console.log(`terminando la validacion`);
       }, 3000);
     }
@@ -39,17 +46,26 @@ class ClassState extends React.Component {
       <div>
         <h2>Eliminar {this.props.name}</h2>
         <p>Por favor, escriba el código de seguridad.</p>
-        {this.state.error && <p>Error: el código es incorrecto</p>}
+        {this.state.error && !this.state.loading && <p>Error: el código es incorrecto</p>}
         {this.state.loading && <p>Cargando...</p>}
-        <input placeholder="Código de seguridad" />
+        <input
+          placeholder="Código de seguridad"
+          value={this.state.value}
+          onChange={(event) => {
+            this.setState({ value: event.target.value });
+          }}
+          disabled={this.state.loading}
+        />
         <button
           onClick={() =>
             this.setState((prevState) => {
               return {
                 loading: !prevState.loading,
+                error:false
               };
             })
           }
+          disabled={this.state.loading}
         >
           Comprobar
         </button>
--- src/UseState.js
+++ src/UseState.js
@@ -1,6 +1,14 @@
 import React from "react";
 
+const SECURITY_CODE = "paradigma";
+
 function UseState({ name }) {
+  const [state, setState] = React.useState({
+    value: "",
+    error: false,
+    loading: false,
+  });
+  const [value, setValue] = React.useState("");
   const [error, setError] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
 
@@ -9,6 +17,9 @@ function UseState({ name }) {
     if (loading) {
       setTimeout(() => {
         console.log(`haciendo la validacion`);
+        if (value !== SECURITY_CODE) {
+          setError(true);
+        }
         setLoading(false);
         console.log(`terminando la validacion`);
       }, 3000);
@@ -20,10 +31,24 @@ function UseState({ name }) {
     <div>
       <h2>Eliminar {name}</h2>
       <p>Por favor, escriba el código de seguridad.</p>
-      {error && <p>Error: el código es incorrecto</p>}
+      {error && !loading && <p>Error: el código es incorrecto</p>}
       {loading && <p>Cargando...</p>}
-      <input placeholder="Código de seguridad" />
-      <button onClick={() => setLoading((prevState) => !prevState)}>
+      <input
+        placeholder="Código de seguridad"
+        value={value}
+        onChange={(event) => {
+          // setError(false);
+          setValue(event.target.value);
+        }}
+        disabled={loading}
+      />
+      <button
+        disabled={loading}
+        onClick={() => {
+          // setError(false);
+          setLoading((prevState) => !prevState);
+        }}
+      >
         Comprobar
       </button>
     </div>
