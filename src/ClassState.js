f113f66659df0c0ead2e491854ea114d01331d3a
Luis Rangel <luis.rangel@telus.com>
2023-03-20 13:17:44 -0600

    feat: metodos del ciclo de vida en Reac.Component

--- src/ClassState.js
+++ src/ClassState.js
@@ -6,7 +6,32 @@ class ClassState extends React.Component {
 
     this.state = {
       error: false,
+      loading: false,
     };
+  }
+
+  // componentWillMount() {
+  //   console.log(`componentWillMount...`)
+  // }
+
+  // componentWillUnmount() {
+  //   console.log(`componentWillUnmount...`)
+  // }
+
+  // componentDidMount() {
+  //   console.log(`componentDidMount...`)
+  // }
+
+  componentDidUpdate() {
+    console.log(`actualizacion`, new Date());
+
+    if (this.state.loading) {
+      setTimeout(() => {
+        console.log(`haciendo la validacion`);
+        this.setState({ loading: false });
+        console.log(`terminando la validacion`);
+      }, 3000);
+    }
   }
 
   render() {
@@ -15,12 +40,13 @@ class ClassState extends React.Component {
         <h2>Eliminar {this.props.name}</h2>
         <p>Por favor, escriba el código de seguridad.</p>
         {this.state.error && <p>Error: el código es incorrecto</p>}
+        {this.state.loading && <p>Cargando...</p>}
         <input placeholder="Código de seguridad" />
         <button
           onClick={() =>
             this.setState((prevState) => {
               return {
-                error: !prevState.error,
+                loading: !prevState.loading,
               };
             })
           }
--- src/UseState.js
+++ src/UseState.js
@@ -2,14 +2,28 @@ import React from "react";
 
 function UseState({ name }) {
   const [error, setError] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    console.log("empezando el efecto");
+    if (loading) {
+      setTimeout(() => {
+        console.log(`haciendo la validacion`);
+        setLoading(false);
+        console.log(`terminando la validacion`);
+      }, 3000);
+    }
+    console.log("terminando el efecto");
+  }, [loading]);
 
   return (
     <div>
       <h2>Eliminar {name}</h2>
       <p>Por favor, escriba el código de seguridad.</p>
       {error && <p>Error: el código es incorrecto</p>}
+      {loading && <p>Cargando...</p>}
       <input placeholder="Código de seguridad" />
-      <button onClick={() => setError((prevState) => !prevState)}>
+      <button onClick={() => setLoading((prevState) => !prevState)}>
         Comprobar
       </button>
     </div>
